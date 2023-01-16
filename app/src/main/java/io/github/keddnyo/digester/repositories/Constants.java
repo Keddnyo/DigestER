3aab1070c3a45745b028f436c66f44f1a9cd28b3
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2023-01-16 19:35:35 +0300

    Translations added

--- app/src/main/java/io/github/keddnyo/digester/repositories/Constants.java
+++ app/src/main/java/io/github/keddnyo/digester/repositories/Constants.java
@@ -1,12 +1,12 @@
 package io.github.keddnyo.digester.repositories;
 
 public class Constants {
-    public static String FORUM_ID = "forum_id";
-    public static String FORUM_TITLE = "forum_title";
-    public static String FORUM_SUBTITLE = "forum_subtitle";
-    public static String RECURSIVE = "recursive";
-    public static String HAS_APPS = "has_apps";
-    public static String RESPONSE = "response";
+    public static final String FORUM_ID = "forum_id";
+    public static final String FORUM_TITLE = "forum_title";
+    public static final String FORUM_SUBTITLE = "forum_subtitle";
+    public static final String RECURSIVE = "recursive";
+    public static final String HAS_APPS = "has_apps";
+    public static final String RESPONSE = "response";
 
-    public static int RESPONSE_TIMEOUT = 30000;
+    public static final int RESPONSE_TIMEOUT = 30000;
 }
